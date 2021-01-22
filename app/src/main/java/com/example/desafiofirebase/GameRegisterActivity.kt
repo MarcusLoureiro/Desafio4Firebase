b75ea010f93d372d0267d5aeac9f9a31c7c26b7d
Marcus Loureiro <marcushuriel08@hotmail.com>
2021-01-22 17:09:16 -0300

    Todos os layouts prontos, firebase storage implementado, mas não é possível enviar dados

--- app/src/main/java/com/example/desafiofirebase/GameRegisterActivity.kt
+++ app/src/main/java/com/example/desafiofirebase/GameRegisterActivity.kt
@@ -2,10 +2,64 @@ package com.example.desafiofirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.app.AlertDialog
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
+import dmax.dialog.SpotsDialog
+import kotlinx.android.synthetic.main.activity_game_register.*
 
 class GameRegisterActivity : AppCompatActivity() {
+    lateinit var alertDialog: AlertDialog
+    lateinit var storageReference: StorageReference
+    private val CODE_IMG = 1000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_register)
+        config()
+        iv_game.setOnClickListener {
+            getRec()
+        }
+    }
+
+    fun config() {
+        alertDialog = SpotsDialog.Builder().setContext(this).build()
+        storageReference = FirebaseStorage.getInstance().getReference("img")
+
+    }
+
+    fun getRec() {
+        val intent = Intent()
+        intent.type = "image/"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Captura Imagem"), CODE_IMG)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CODE_IMG) {
+            alertDialog.show()
+            val uploadFile = storageReference.putFile(data!!.data!!)
+            val task = uploadFile.continueWithTask { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Imagem Carrregada com sucesso!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                storageReference!!.downloadUrl
+            }.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val downloadUri = task.result
+                    val url = downloadUri!!.toString()
+                        .substring(0, downloadUri.toString().indexOf("&token"))
+                    Log.i("URL da Imagem", url)
+                    alertDialog.dismiss()
+                    Picasso.get().load(url).into(iv_game)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/desafiofirebase/MainActivity.kt
+++ app/src/main/java/com/example/desafiofirebase/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.desafiofirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_game_register.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fb_addGame.setOnClickListener {
+            callRegisterGame()
+        }
+
+    }
+
+    fun callRegisterGame(){
+        val intent = Intent(this, GameRegisterActivity::class.java)
+        startActivity(intent)
     }
 }
