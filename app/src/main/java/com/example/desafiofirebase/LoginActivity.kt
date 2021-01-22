ac6374ea87b613134f84d87c5b3c5a954f0a16d8
Marcus Loureiro <marcushuriel08@hotmail.com>
2021-01-22 11:17:54 -0300

    Layouts Splash, Login, Register prontos

---
+++ app/src/main/java/com/example/desafiofirebase/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.example.desafiofirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        tvRegister.setOnClickListener {
+            callRegister()
+        }
+
+        btLogin.setOnClickListener {
+            callHome()
+        }
+    }
+
+    fun callRegister (){
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun callHome(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/desafiofirebase/RegisterActivity.kt
@@ -0,0 +1,24 @@
+package com.example.desafiofirebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        btRegister.setOnClickListener {
+            callHome()
+        }
+
+    }
+
+
+    fun callHome(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
